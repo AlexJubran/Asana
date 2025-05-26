afbd65afb59dce481f7053209a81477699d3c6f4
Alex <jubranac71@gmail.com>
2025-05-26 10:31:47 -0400

    Add project class

---
+++ Asana.Library/Models/Project.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Library.Models
+{
+    public class Project
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double? CompletePercent { get; set; }
+        public List<int> toDoIDs { get; set; } = new List<int>();
+
+        public override string ToString()
+        {
+            return $"{Name} - {Description} - {listToDos()}";
+        }
+
+        public string listToDos()
+        {
+            string todos = string.Empty;
+            foreach (var item in toDoIDs)
+            {
+                todos += item.ToString() ;
+            }
+
+            return todos;
+        }
+    }
+}
